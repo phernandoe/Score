21d823029806bcce49fe3c750e418c1da8601411
Phernandoe <fbarreto@ithaca.edu>
2019-05-08 01:08:09 -0400

    Working login

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
   $("form").submit(function() {
       if ($('#username').val() === 'admin' && $('#password').val() === 'admin'){
-          window.location.replace("http://www.google.com/");
+          window.location = ("index");
       } else {
           alert("Login unsuccessful");
       }
+      return false;
   });
 
 });
